fb11e49637a91013ebc633c03b630d24e0dbd3df
morganmark <32056855+morganmark@users.noreply.github.com>
2020-07-02 22:28:18 +0800

    AR

---
+++ AR/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //float ran = Random.Range(-50.0f, 50.0f);
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // GameObject.Find("Cube").transform.Rotate(0, 0,f);
+    }
+
+    public void left()
+    {
+        GameObject.Find("Cube").transform.Rotate(0, 0, 5f);
+    }
+    public void right()
+    {
+        GameObject.Find("Cube").transform.Rotate(0, 0, -5f);
+    }
+
+    public void front()
+    {
+        GameObject.Find("Cube").transform.Rotate(-5f, 0, 0);
+    }
+
+    public void back()
+    {
+        GameObject.Find("Cube").transform.Rotate(5f, 0, 0);
+    }
+}
---
+++ AR/Assets/Scenes/NewBehaviourScript1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    Rigidbody rigid;
+    public float force = 5;
+
+    void Start()
+    {
+        rigid = transform.GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        rigid.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * force);
+    }
+}
+
